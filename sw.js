cc3aeeb6f16fee70b17a6ac79f646711861cb0b2
jackjeanab <jackjeanab@users.noreply.github.com>
2026-09-09 21:17:06 +0800

    Add GitHub Pages PWA launcher

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'student-score-launcher-v1';
+const SHELL = './';
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './manifest.webmanifest', './icon.svg'])).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
